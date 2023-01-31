b49a06a9fa34d2d4388b69def180f8f0972ae3a5
TISWHORDweb <ebatimehin@gmail.com>
2023-01-31 20:39:04 +0100

    cors issue

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app =express()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoroWaitlistRoute = require('./routes/mongoro/waitlist/api')
 const reefWaitlistRoute = require('./routes/reef/waitlist/api')
@@ -12,14 +13,14 @@ const mongoroTransaction = require('./routes/mongoro/Transaction/api')
 const mongoroTickets = require('./routes/mongoro/Tickets/api')
 const mongoroSuperAdminCategory = require('./routes/mongoro/Admin/super/category/category')
 const mongoose=require('mongoose')
-const cors = require('cors')
+
 // const dotenv = require("dotenv")
 // dotenv.config()
 
 
 app.use(bodyParser.json())
 
-app.use(cors())
+app.use(cors({origin: "*"}))
 
 app.use("/mongoro_waitlist", mongoroWaitlistRoute)
 app.use("/reef_waitlist", reefWaitlistRoute)
