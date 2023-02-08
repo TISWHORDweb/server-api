d86b87952bbaabe899368976046dfe402e352e41
Emmanuel <ebatimehin@gmail.com>
2023-02-08 15:42:47 +0100

    status cors rem

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ const mongoose=require('mongoose')
 // const dotenv = require("dotenv")
 // dotenv.config()
 
-app.use(cors({"origin":"*"}))
+// app.use(cors({"origin":"*"}))
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
+    res.header("mode","no-cors")
+ 
 })
 
 app.use(bodyParser.json())
