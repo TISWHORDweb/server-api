c6a98cda49fe4cb6a4623f96408fb40366b75ef4
Emmanuel <ebatimehin@gmail.com>
2023-02-08 15:33:20 +0100

    status

--- routes/mongoro/auth/api.js
+++ routes/mongoro/auth/api.js
@@ -201,9 +201,9 @@ router.post("/login", async (req, res) => {
 
     if (user === null) {
         console.log("User does not exists");
-        res.status(401).json("wrong password or username !");
+        res.status(400).json("wrong password or username !");
     } else if (originalPassword != req.body.password) {
-        res.status(401).json({ msg: 'wrong password !', status: 401 });
+        res.status(400).json({ msg: 'wrong password !', status: 400 });
     } else {
         const accessToken = jwt.sign(
             { id: user._id, isverified: user.isverified },
