f44fe81999036de51d55594f8dbd41cd70206b32
Emmanuel <ebatimehin@gmail.com>
2023-03-01 15:33:08 +0100

    admin login

--- routes/mongoro/auth/api.js
+++ routes/mongoro/auth/api.js
@@ -151,15 +151,18 @@ router.post("/login", async (req, res) => {
     const users = await GlobalModel.findOne({ _id: process.env.GLOBAL_ID })
     const value = users.disable_all_user
     const user = await MongoroUserModel.findOne({ email: req.body.email })
+
+    if (!user) {
+        res.status(400).json({ msg: "user not found", code: 400 })
+    }
+
     const resultt = user.blocked
 
-   if (!user) {
-        res.status(400).json({ msg: "user not found", code: 400 })
-    } else  if (resultt === true) {
+    if (resultt === true) {
         res.status(403).json({ msg: "Sorry your account is blocked", code: 403 })
     } else if (value === true) {
         res.status(500).json({ msg: "Sorry service temporarily unavailable", code: 500 })
-    }  else {
+    } else {
         const originalPassword = await bcrypt.compare(req.body.password, user.password);
 
         if (!originalPassword) {
@@ -181,6 +184,74 @@ router.post("/login", async (req, res) => {
 
 })
 
+router.post("/admin_login", async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const users = await GlobalModel.findOne({ _id: process.env.GLOBAL_ID })
+    const value = users.disable_all_user
+    const user = await MongoroUserModel.findOne({ email: req.body.email })
+
+    if (!user) {
+        res.status(400).json({ msg: "user not found", code: 400 })
+    } 
+    
+    const resultt = user.blocked
+
+    if (user.category === "Admin") {
+        if (resultt === true) {
+            res.status(403).json({ msg: "Sorry your account is blocked", code: 403 })
+        } else if (value === true) {
+            res.status(500).json({ msg: "Sorry service temporarily unavailable", code: 500 })
+        } else {
+
+            const originalPassword = await bcrypt.compare(req.body.password, user.password);
+
+            if (!originalPassword) {
+                res.status(400).json({ msg: "wrong password", code: 400 })
+            } else {
+                const accessToken = jwt.sign(
+                    { id: user._id, isverified: user.isverified },
+                    process.env.SECRET_KEY,
+                    { expiresIn: "5h" }
+                );
+
+                const ip = address.ip();
+
+                await MongoroUserModel.updateOne({ _id: user._id }, { $set: { ip: ip } }).then(() => {
+                    res.status(200).json({ msg: 'logged in successfuly Admin !',category:"Admin", user: user, token: accessToken, ip_address: ip, status: 200 });
+                })
+            }
+        }
+
+    } else if (user.category === "Super Admin") {
+
+        if (resultt === true) {
+            res.status(403).json({ msg: "Sorry your account is blocked", code: 403 })
+        } else {
+
+            const originalPassword = await bcrypt.compare(req.body.password, user.password);
+
+            if (!originalPassword) {
+                res.status(400).json({ msg: "wrong password", code: 400 })
+            } else {
+                const accessToken = jwt.sign(
+                    { id: user._id, isverified: user.isverified },
+                    process.env.SECRET_KEY,
+                    { expiresIn: "5h" }
+                );
+
+                const ip = address.ip();
+
+                await MongoroUserModel.updateOne({ _id: user._id }, { $set: { ip: ip } }).then(() => {
+                    res.status(200).json({ msg: 'logged in successfuly Super Admin!', category:"Super Admin", user: user, token: accessToken, ip_address: ip, status: 200 });
+                })
+            }
+        }
+
+    } else {
+        res.status(403).json({ msg: "Sorry you have No access ", code: 403 })
+    }
+})
 
 //FORGOTPASSWORD 
 router.post("/emailverify", async (req, res) => {
