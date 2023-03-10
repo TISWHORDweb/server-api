cf1c3665b7b4de6db28daf9e21b417528f3712e9
Emmanuel <ebatimehin@gmail.com>
2023-03-10 14:27:01 +0100

    Driver license validation

--- routes/mongoro/auth/kyc.js
+++ routes/mongoro/auth/kyc.js
@@ -73,12 +73,8 @@ router.post('/nin', async (req, res) => {
                 } else {
                     console.log({ msg: "All details match " })
 
-                    if (req.body.auth_id) {
-                        req.body.auth_id = bcrypt.hash(req.body.auth_id, 13)
-                    }
-
                     const bodys = {
-                        // "b_id": req.body.b_id,
+                        "auth_id": bcrypt.hash(req.body.auth_id, 13),
                         "type":req.body.type,
                         "userId":req.body.userId,
                         "check": "MON" + check + "GORO",
@@ -172,12 +168,103 @@ router.post('/passport', async (req, res) => {
                 } else {
                     console.log({ msg: "All details match " })
 
-                    if (req.body.auth_id) {
-                        req.body.auth_id = bcrypt.hash(req.body.auth_id, 13)
+                    const bodys = {
+                        "auth_id": bcrypt.hash(req.body.auth_id, 13),_id,
+                        "type":req.body.type,
+                        "userId":req.body.userId,
+                        "check": "MON" + check + "GORO",
+                        "data": resp.data,
+                        "image": req.body.image,
+                        "expire_at": req.body.expire_at
                     }
 
+                    let details = new KycModel(bodys)
+                    details.save()
+                    res.send(details)
+                    // MongoroUserModel.updateOne({ _id: req.body.userId }, { $set: { verification: { kyc: true } } })
+
+                }
+
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: 'There is an unknown error sorry.... Please contact our support ',
+            status: 500
+        })
+    }
+})
+
+
+//DRIVER LICENSE
+router.post('/driver_license', async (req, res) => {
+
+    const id = req.body.userId
+    const lastName = req.body.lastName
+    const firstName = req.body.firstName
+
+    const check = req.body.auth_id.substr(req.body.auth_id.length - 5)
+    console.log(check)
+
+    const url = "https://api.sandbox.youverify.co/v2/api/identity/ng/drivers-license"
+
+    const header = {
+        headers: {
+            token: process.env.U_VERIFY_KEY
+        }
+    }
+    try {
+
+        const validate = await KycModel.findOne({ check: "MON" + check + "GORO" })
+
+        function ent() {
+            const checking = validate.data.data
+
+            if (checking.firstName !== firstName) {
+                res.status(400).json({
+                    msg: 'Credentials does not match !',
+                    status: 400
+                })
+            } else if (checking.lastName !== lastName) {
+                res.status(400).json({
+                    msg: 'Credentials does not match !',
+                    status: 400
+                })
+            } 
+        }
+
+        if (validate) {
+            ent()
+
+            res.send({verified:"Verified before",validate})
+        } else {
+            console.log("account")
+
+            await axios.post(url, {
+                "id": req.body.auth_id,
+                "isSubjectConsent": true,
+                "validations": {
+                    "data": {
+                        "lastName": req.body.lastName,
+                        "firstName": req.body.firstName,
+                    }
+                }
+            }, header).then(resp => {
+                const data = resp.data.data
+                if (!data) {
+                    res.status(400).json({ msg: `Invalid ${req.body.type}` })
+                }
+                if (data.lastName !== lastName) {
+                    res.status(400).json({ msg: 'Credentials does not match ?' })
+                } else if (data.firstName !== firstName) {
+                    res.status(400).json({ msg: 'Credentials does not match ?' })
+                } else {
+                    console.log({ msg: "All details match " })
+
                     const bodys = {
-                        // "b_id": req.body.b_id,
+                        "auth_id": bcrypt.hash(req.body.auth_id, 13),
                         "type":req.body.type,
                         "userId":req.body.userId,
                         "check": "MON" + check + "GORO",
