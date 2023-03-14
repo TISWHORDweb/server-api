08cd1bad7e9b56090935d953850f690aa5fe23ea
Emmanuel <ebatimehin@gmail.com>
2023-03-14 17:07:36 +0100

    email updates

--- routes/mongoro/Admin/admin.js
+++ routes/mongoro/Admin/admin.js
@@ -84,9 +84,9 @@ router.post("/login", async (req, res) => {
                                                                 <hr 
                                                                     style='border: none; border-bottom: 0.6px solid #FFF7E6'
                                                                 />
-                                                                <p style='color: #666666; text-align: center; font-size: 14px; margin: 2rem 0 0 0'>+234 09169451169</p>
-                                                                <p style='color: #666666; text-align: center; font-size: 14px;'>sales@mongoro.com</p>
-                                                                <p style='color: #666666; text-align: center; font-size: 14px;'>Space 27, Novare Mall, Wuse Zone 5, Abuja</p>
+                                                                <p style='color: #666666; text-align: center; font-size: 14px; margin: 2rem 0 0 0'>www.mongoro.com</p>
+                                                                <p style='color: #666666; text-align: center; font-size: 14px;'>support@mongoro.com</p>
+                                                                <p style='color: #666666; text-align: center; font-size: 14px;'>21 Blantyre Crescent, wuse 2, Abuja</p>
                                                                 <p style='color: #666666; text-align: center; font-size: 14px; margin: 2rem 0 0 0'> Having trouble viewing this email? Click here to view in your browser.</p>
                                                         </td>
                                                     </tr>
