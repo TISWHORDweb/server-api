8500ec97ccec16d7903975d4c54a3d0157235575
James Favour <68597611+Sage-Ops-Core@users.noreply.github.com>
2023-04-17 16:01:52 +0100

    login activity

--- test/total.test.js
+++ test/total.test.js
@@ -1,21 +1,15 @@
-const TransferModel = require("../models/mongoro/transaction/api");
-try {
-    TransferModel.aggregate([{
-        $group: {
-            _id: null,
-            "TotalTransaction": {
-                '$sum': {
-                    '$convert': {'input': '$amount', 'to': 'int'},
-                    '$toDate': Date.now()
-                }
-            }
-        }
-    }]).then(rt => {
-        console.log(rt)
-    }).catch(e => {
-        console.error(e)
-    })
-}catch (e) {
-    console.error(e)
+const address = require("address");
+const ipLocation = require("iplocation");
+
+const ip = address.ip();
+
+async function getUserLocation(ip) {
+    const response = await fetch(`https://ipinfo.io/${ip}/json?token=26917d1f7f2cb9`);
+    const {city, country} = await response.json();
+    return {city, country};
 }
 
+// Example usage:
+getUserLocation(ip)
+    .then(location => console.log(location))
+    .catch(error => console.error(error));
