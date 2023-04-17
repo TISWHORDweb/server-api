bb1b6ccbccb45d37e8c38f87c3718a01495f9e72
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2023-04-17 12:14:56 +0100

    cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const expressLayouts = require('express-ejs-layouts');
+// const expressLayouts = require('express-ejs-layouts');
 const cors = require('cors')
 const path = require('path');
 const bodyParser = require('body-parser')
@@ -37,10 +37,17 @@ const mongoose = require('mongoose')
 // const dotenv = require("dotenv")
 // dotenv.config()
 
-// app.use(cors({"origin":"*"}))
-app.use(cors({"origin": "*"}))
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// app.use(expressLayouts);
+// app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/docs', express.static(path.join(__dirname, 'docs')));
 // app.use(function(req, res, next) {
