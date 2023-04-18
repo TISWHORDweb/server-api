2531a3c72358a37ae5eb4cf649c9054ab6fd6d8f
James Favour <68597611+Sage-Ops-Core@users.noreply.github.com>
2023-04-18 13:00:35 +0100

    totals

--- routes/mongoro/total/total.js
+++ routes/mongoro/total/total.js
@@ -9,350 +9,350 @@ const TicketModel = require('../../../models/mongoro/tickets/api')
 router.get("/totals", async (req, res) => {
     try {
 
-    /*
-    Totals based on dates
-     */
-    const oneDayAgoTimeStamp = Date.now() - (24 * 60 * 60 * 1000);
-    const oneWeekAgoTimeStamp = Date.now() - (7 * 24 * 60 * 60 * 1000);
-    const oneMonthAgoTimeStamp = Date.now() - (30 * 24 * 60 * 60 * 1000);
+        /*
+        Totals based on dates
+         */
+        const oneDayAgoTimeStamp = Date.now() - (24 * 60 * 60 * 1000);
+        const oneWeekAgoTimeStamp = Date.now() - (7 * 24 * 60 * 60 * 1000);
+        const oneMonthAgoTimeStamp = Date.now() - (30 * 24 * 60 * 60 * 1000);
 
 
-    //--------------------------------Deposits------------------------------
+        //--------------------------------Deposits------------------------------
 
-    const totalDeposit = await TransferModel.aggregate([
-        {
-            $match: {
-                service_type: "Deposit",
-            }
-        },
+        const totalDeposit = await TransferModel.aggregate([
+            {
+                $match: {
+                    service_type: "Deposit",
+                }
+            },
 
-        {
-            $group: {
-                _id: null,
-                "total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+            {
+                $group: {
+                    _id: null,
+                    "total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    //TODAY"S DEPOSIT
-    const dailyDeposit = await TransferModel.aggregate([
-        {
-            $match: {
-                service_type: "Deposit",
-                Date: {
-                    $gte: oneDayAgoTimeStamp
+        //TODAY"S DEPOSIT
+        const dailyDeposit = await TransferModel.aggregate([
+            {
+                $match: {
+                    service_type: "Deposit",
+                    Date: {
+                        $gte: oneDayAgoTimeStamp
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            $group: {
-                _id: null,
-                "total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+            {
+                $group: {
+                    _id: null,
+                    "total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    //THIS WEEK"S DEPOSIT
-    const weeklyDeposit = await TransferModel.aggregate([
-        {
-            $match: {
-                Date: {
-                    $gte: oneWeekAgoTimeStamp
-                },
-                service_type: "Deposit"
-            }
-        },
-        {
-            $group: {
-                _id: null,
-                "Total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+        //THIS WEEK"S DEPOSIT
+        const weeklyDeposit = await TransferModel.aggregate([
+            {
+                $match: {
+                    Date: {
+                        $gte: oneWeekAgoTimeStamp
+                    },
+                    service_type: "Deposit"
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    "Total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    //THIS MONTH"S DEPOSITS
-    const monthlyDeposit = await TransferModel.aggregate([
-        {
-            $match: {
-                Date: {
-                    $gte: oneMonthAgoTimeStamp
-                },
-                service_type: "Deposit"
-            }
-        },
-        {
-            $group: {
-                _id: null,
-                "Total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+        //THIS MONTH"S DEPOSITS
+        const monthlyDeposit = await TransferModel.aggregate([
+            {
+                $match: {
+                    Date: {
+                        $gte: oneMonthAgoTimeStamp
+                    },
+                    service_type: "Deposit"
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    "Total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    //--------------------------------Withdrawals------------------------------
+        //--------------------------------Withdrawals------------------------------
 
-    const totalWithdrawal = await TransferModel.aggregate([
-        {
-            $match: {
-                service_type: "Transfer",
-            }
-        },
+        const totalWithdrawal = await TransferModel.aggregate([
+            {
+                $match: {
+                    service_type: "Transfer",
+                }
+            },
 
-        {
-            $group: {
-                _id: null,
-                "total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+            {
+                $group: {
+                    _id: null,
+                    "total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    //TODAY"S WITHDRAWALS
-    const dailyWithdrawal = await TransferModel.aggregate([
-        {
-            $match: {
-                service_type: "Transfer",
-                Date: {
-                    $gte: oneDayAgoTimeStamp
+        //TODAY"S WITHDRAWALS
+        const dailyWithdrawal = await TransferModel.aggregate([
+            {
+                $match: {
+                    service_type: "Transfer",
+                    Date: {
+                        $gte: oneDayAgoTimeStamp
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            $group: {
-                _id: null,
-                "total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+            {
+                $group: {
+                    _id: null,
+                    "total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    // THIS WEEK"S WITHDRAWALS
-    const weeklyWithdrawal = await TransferModel.aggregate([
-        {
-            $match: {
-                service_type: "Transfer",
-                Date: {
-                    $gte: oneWeekAgoTimeStamp
+        // THIS WEEK"S WITHDRAWALS
+        const weeklyWithdrawal = await TransferModel.aggregate([
+            {
+                $match: {
+                    service_type: "Transfer",
+                    Date: {
+                        $gte: oneWeekAgoTimeStamp
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            $group: {
-                _id: null,
-                "total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+            {
+                $group: {
+                    _id: null,
+                    "total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    // THIS MONTH"S WITHDRAWALS
-    const monthlyWithdrawal = await TransferModel.aggregate([
-        {
-            $match: {
-                service_type: "Transfer",
-                Date: {
-                    $gte: oneMonthAgoTimeStamp
+        // THIS MONTH"S WITHDRAWALS
+        const monthlyWithdrawal = await TransferModel.aggregate([
+            {
+                $match: {
+                    service_type: "Transfer",
+                    Date: {
+                        $gte: oneMonthAgoTimeStamp
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            $group: {
-                _id: null,
-                "total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+            {
+                $group: {
+                    _id: null,
+                    "total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    //--------------------------------Bills------------------------------
+        //--------------------------------Bills------------------------------
 
-    const totalBill = await TransferModel.aggregate([
-        {
-            $match: {
-                service_type: "Bills",
-            }
-        },
+        const totalBill = await TransferModel.aggregate([
+            {
+                $match: {
+                    service_type: "Bills",
+                }
+            },
 
-        {
-            $group: {
-                _id: null,
-                "total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+            {
+                $group: {
+                    _id: null,
+                    "total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    //TODAY"S BILLS
-    const dailyBill = await TransferModel.aggregate([
-        {
-            $match: {
-                service_type: "Bills",
-                Date: {
-                    $gte: oneDayAgoTimeStamp
+        //TODAY"S BILLS
+        const dailyBill = await TransferModel.aggregate([
+            {
+                $match: {
+                    service_type: "Bills",
+                    Date: {
+                        $gte: oneDayAgoTimeStamp
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            $group: {
-                _id: null,
-                "total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+            {
+                $group: {
+                    _id: null,
+                    "total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    // THIS WEEK"S BILLS
-    const weeklyBill = await TransferModel.aggregate([
-        {
-            $match: {
-                service_type: "Bills",
-                Date: {
-                    $gte: oneWeekAgoTimeStamp
+        // THIS WEEK"S BILLS
+        const weeklyBill = await TransferModel.aggregate([
+            {
+                $match: {
+                    service_type: "Bills",
+                    Date: {
+                        $gte: oneWeekAgoTimeStamp
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            $group: {
-                _id: null,
-                "total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+            {
+                $group: {
+                    _id: null,
+                    "total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    // THIS MONTH"S BILLS
-    const monthlyBill = await TransferModel.aggregate([
-        {
-            $match: {
-                service_type: "Bills",
-                Date: {
-                    $gte: oneMonthAgoTimeStamp
+        // THIS MONTH"S BILLS
+        const monthlyBill = await TransferModel.aggregate([
+            {
+                $match: {
+                    service_type: "Bills",
+                    Date: {
+                        $gte: oneMonthAgoTimeStamp
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            $group: {
-                _id: null,
-                "total": {
-                    '$sum': {
-                        '$convert': {'input': '$amount', 'to': 'int'}
+            {
+                $group: {
+                    _id: null,
+                    "total": {
+                        '$sum': {
+                            '$convert': {'input': '$amount', 'to': 'int'}
+                        }
                     }
                 }
             }
-        }
-    ])
+        ])
 
-    /*
-    END Totals based on dates
-     */
+        /*
+        END Totals based on dates
+         */
 
-    //TOTAL USER
-    const count = await MongoroUserModel.countDocuments();
+        //TOTAL USER
+        const count = await MongoroUserModel.countDocuments();
 
-    //TOTAL TRANSACTION
-    // const transaction = await TransferModel.aggregate([{
-    //     $match: {
-    //         service_type: "Transfer"
-    //     }
-    // }, {
-    //     $group: {
-    //         _id: null,
-    //         "TotalTransaction": {
-    //             '$sum': {
-    //                 '$convert': {'input': '$amount', 'to': 'int'}
-    //             }
-    //         }
-    //     }
-    // }])
+        //TOTAL TRANSACTION
+        // const transaction = await TransferModel.aggregate([{
+        //     $match: {
+        //         service_type: "Transfer"
+        //     }
+        // }, {
+        //     $group: {
+        //         _id: null,
+        //         "TotalTransaction": {
+        //             '$sum': {
+        //                 '$convert': {'input': '$amount', 'to': 'int'}
+        //             }
+        //         }
+        //     }
+        // }])
 
-    //TOTAL SAVING
-    const saving = await MongoroUserModel.aggregate([{
-        $group: {
-            _id: null,
-            "TotalSaving": {
-                '$sum': {
-                    '$convert': {'input': '$wallet_balance', 'to': 'int'}
+        //TOTAL SAVING
+        const saving = await MongoroUserModel.aggregate([{
+            $group: {
+                _id: null,
+                "TotalSaving": {
+                    '$sum': {
+                        '$convert': {'input': '$wallet_balance', 'to': 'int'}
+                    }
                 }
             }
-        }
-    }])
+        }])
 
-    //ACTIVE USER
-    const active = await MongoroUserModel.find({active: true})
+        //ACTIVE USER
+        const active = await MongoroUserModel.find({active: true})
 
-    //INACTIVE USER
-    const inactive = await MongoroUserModel.find({active: false})
+        //INACTIVE USER
+        const inactive = await MongoroUserModel.find({active: false})
 
-    //TICKETS
-    const ticket = await TicketModel.countDocuments();
+        //TICKETS
+        const ticket = await TicketModel.countDocuments();
 
 
-    res.status(200).json({
-        Total_user: count,
-        transaction,
-        totalDeposit,
-        dailyDeposit,
-        weeklyDeposit,
-        monthlyDeposit,
-        totalWithdrawal,
-        dailyWithdrawal,
-        weeklyWithdrawal,
-        monthlyWithdrawal,
-        totalBill,
-        dailyBill,
-        weeklyBill,
-        monthlyBill,
-        saving,
-        TotalActive: active.length,
-        TotalInactive: inactive.length,
-        Total_tickets: ticket
-    });
+        res.status(200).json({
+            Total_user: count,
+            totalDeposit,
+            dailyDeposit,
+            weeklyDeposit,
+            monthlyDeposit,
+            totalWithdrawal,
+            dailyWithdrawal,
+            weeklyWithdrawal,
+            monthlyWithdrawal,
+            totalBill,
+            dailyBill,
+            weeklyBill,
+            monthlyBill,
+            saving,
+            TotalActive: active.length,
+            TotalInactive: inactive.length,
+            Total_tickets: ticket
+        });
     } catch (err) {
         res.status(500).json({
             msg: 'there is an unknown error sorry !',
+            err: err.message,
             status: 500
         })
     }
